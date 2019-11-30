0022aebfa45d875ceb22547b6321dd4bfc516113
Andrei Ciuvaga <andrei.ciuvaga@gmail.com>
2019-12-01 01:32:56 +0200

    First Commit

---
+++ src/main/java/com/application/junit/MathUtils.java
@@ -0,0 +1,8 @@
+package com.application.junit;
+
+public class MathUtils {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/test/java/com/application/junit/MathUtilsTest.java
@@ -0,0 +1,13 @@
+package com.application.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    @Test
+    void add() {
+        System.out.println("This test ran");
+    }
+}
